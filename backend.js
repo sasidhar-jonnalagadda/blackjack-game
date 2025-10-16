2b306297c8e7456c4e9b4b38f5d75bd194de148f
Sasidhar Jonnalagadda <sasidharj1234@gmail.com>
2025-10-16 22:42:30 +0530

    Add 3 new functions
    Added startGame(), newGame(), and getRandomCard()

--- backend.js
+++ backend.js
@@ -1,6 +1,7 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
+let sum = cards[0] + cards[1];
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
@@ -9,18 +10,36 @@ let messageEl = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
 
-function startGame() {
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
 
   messageEl.textContent = message;
+}
+
+function startGame() {
+  renderGame();
+}
+
+function newGame() {
+  let card = getRandomCard();
+  cards.push(card);
+  sum += card;
+  renderGame();
+}
+
+function getRandomCard() {
+  return 5;
 }
